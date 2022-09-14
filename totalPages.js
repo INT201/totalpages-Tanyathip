547c3ce8b212e4d8c81719eb8cd95115119ce03b
Tanyathip <Tanyathip.aoongern@mail.kmutt.ac.th>
2022-09-15 00:33:22 +0700

    Homework

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,22 @@
 const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if(rowsPerPage == null || undefined){
+    // console.log(1)
+    return 1
+  }
+  else if(arrayItems == null || undefined){
+    // console.log(undefined)
+    return undefined
+  }
+  else{
+    page = arrayItems.length/rowsPerPage
+    if(page != Infinity){
+      return Math.ceil(page)
+    }
+    else{return 1}
+    // console.log(page)
+  }
+
 }
 module.exports = totalPages
